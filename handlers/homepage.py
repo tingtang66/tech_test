4ac09e0ebcd8ade15d8fabd8f671019e54ff4cd8
Naiguang Xu <naiguang.xu@gmail.com>
2016-12-04 21:25:18 +0000

    Hot fix

--- handlers/homepage.py
+++ handlers/homepage.py
@@ -3,10 +3,6 @@ import subprocess
 
 class IndexHandler(RequestHandler):
     def get(self):
-    	interfaces = subprocess.Popen(['ip', 'addr'], stdout=subprocess.PIPE)
-    	eth1_brief = subprocess.Popen(['grep', 'global eth1'], stdin=interfaces.stdout, stdout=subprocess.PIPE)
-    	interfaces.stdout.close()
-    	output = eth1_brief()[0]
-        print(output)
-        self.render("index.html", output=output)
+        uri = self.request.uri
+        self.render("index.html", output=uri)
 
