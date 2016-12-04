0e2514bc6abe9ecc746c87fa809031305764a35b
Naiguang Xu <naiguang.xu@gmail.com>
2016-12-04 22:42:25 +0000

    add environment var

--- handlers/homepage.py
+++ handlers/homepage.py
@@ -1,9 +1,10 @@
+import os
 from tornado.web import RequestHandler
 from handlers import Env
 
 class IndexHandler(RequestHandler):
     def get(self):
-        host_ip = self.request.host
+        host_ip = os.environ['HOST_IP']
         template = Env.get_template('index.j2')
         self.write(template.render(output=host_ip))
 
