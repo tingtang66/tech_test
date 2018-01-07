5b2b6aa39ec67e1108950b18b3408f632d57a4be
Nicholas Xu <tingtang66@gmail.com>
2018-01-07 19:00:47 +0000

    Updated

--- handlers/homepage.py
+++ handlers/homepage.py
@@ -1,10 +1,11 @@
 import os
 from tornado.web import RequestHandler
 from handlers import Env
+from subprocess import check_output
 
 class IndexHandler(RequestHandler):
     def get(self):
-        host_ip = os.environ['HOST_IP']
+        host_ip = check_output(['curl', 'http://169.254.169.254/latest/meta-data/public-ipv4'])
         template = Env.get_template('index.j2')
         self.write(template.render(output=host_ip))
 
